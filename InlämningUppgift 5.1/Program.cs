c260b8893cfb47dd23ccadd6a692d68d4f1f043d
dean06001 <dean06001@E-PW0234KC>
2023-11-22 09:54:32 +0100

    Add project files.

---
+++ InlämningUppgift 5.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace InlämningUppgift_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 5 namn");
+
+            string[] namn = new string[5];
+
+            for (int i = 0; i < namn.Length; i++)
+            {
+                Console.WriteLine($"Skriv in namn {i + 1}");
+                namn[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Här är de namnen du skrev in");
+
+            foreach (string ord in namn)
+            {
+                Console.WriteLine(ord);
+            }
+            Console.ReadLine();
+        }
+    }
+}
